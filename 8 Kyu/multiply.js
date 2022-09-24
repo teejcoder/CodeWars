91e69543a86b581dc14cb46b2011f9496d814bad
teejcoder <teejcoder@gmail.com>
2022-09-24 12:23:40 +1000

    Create multiply.js

---
+++ 8 Kyu/multiply.js
@@ -0,0 +1,11 @@
+//This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//     a * b
+//   }
+
+
+
+//my solution
+// function multiply(a, b){
+//     return a * b
+//   }
