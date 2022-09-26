20f777e44715302a97aa4dc2dc2bf840ca25404b
teejcoder <teejcoder@gmail.com>
2022-09-27 08:49:20 +1000

    Remove string spaces

---
+++ 8 Kyu/removing-string-spaces.js
@@ -0,0 +1,10 @@
+//Removing String Spaces
+//Simple, remove the spaces from the string, then return the resultant string.
+
+//my code
+// function noSpace(x){
+  
+//     return x.replace(/\s/g,'')
+      
+//     }
+    
