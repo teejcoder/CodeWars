f1e65587f3d97ddd108ed1d6ed247963e840268d
teejcoder <teejcoder@gmail.com>
2022-10-07 14:00:24 +1100

    latest

---
+++ 8 Kyu/reversed-strings.js
@@ -0,0 +1,16 @@
+/* DESCRIPTION
+
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+
+/* MY CODE
+
+function solution(str){
+  return str.split('').reverse().join('')
+}
+
+*/
