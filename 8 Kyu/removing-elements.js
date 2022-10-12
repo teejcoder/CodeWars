5ea54b483ec1a07a3134ec7ee7ac774d540defeb
teejcoder <teejcoder@gmail.com>
2022-10-12 14:48:34 +1100

    Create removing-elements.js

---
+++ 8 Kyu/removing-elements.js
@@ -0,0 +1,11 @@
+/* DESCRIPTION
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+*/
+
+/* MY CODE
+function removeEveryOther(arr){
+  return arr.filter(function(elem, index){
+    return index % 2 == 0
+  })
+}
+*/
