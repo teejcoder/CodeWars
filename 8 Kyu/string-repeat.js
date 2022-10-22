0dca2cf4377c0713d201b8808634408de977ff9b
teejcoder <teejcoder@gmail.com>
2022-10-22 11:24:22 +1100

    Create string-repeat.js

---
+++ 8 Kyu/string-repeat.js
@@ -0,0 +1,12 @@
+/* DESCRIPTION
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+*/
+
+
+/* MY CODE
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
+
+*/
