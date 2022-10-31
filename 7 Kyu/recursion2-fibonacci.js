5530c854bef062ace42b7445c2f32e0869581fe5
Tristan Maber <teejcoder@gmail.com>
2022-10-31 17:20:37 +1100

    Create recursion2-fibonacci.js

---
+++ 7 Kyu/recursion2-fibonacci.js
@@ -0,0 +1,14 @@
+/* DESCRIPTION
+You have to create the function fibonacci that receives n and returns f(n). You have to use recursion.
+*/
+
+/* MY CODE
+const fibonacci = n => {
+  if (n === 1){
+    return 1
+  } else if (n === 2){
+    return 1
+  } 
+  return fibonacci (n - 1) + fibonacci (n - 2)
+};
+*/
