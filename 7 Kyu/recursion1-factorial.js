75df0102c03dc93b2e82b3444a297f8811657701
Tristan Maber <teejcoder@gmail.com>
2022-10-31 16:54:33 +1100

    Create recursion1-factorial.js

---
+++ 7 Kyu/recursion1-factorial.js
@@ -0,0 +1,15 @@
+/* DESCRIPTION
+You have to create the function factorial that receives n and returns n!. You have to use recursion.
+*/
+
+
+/* MY CODE
+
+const factorial = n => {
+  if (n === 0){
+    return 1
+  }
+  return n * factorial (n - 1)
+};
+
+*/
