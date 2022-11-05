e2cc23e5a8ba6bf76d05f75aecffd855bd85d4d8
Tristan Maber <teejcoder@gmail.com>
2022-11-05 13:17:28 +1100

    Create remove-exclamation-marks.js

---
+++ 8 Kyu/remove-exclamation-marks.js
@@ -0,0 +1,11 @@
+/* DESCRIPTION
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+*/
+
+
+/* MY CODE
+function removeExclamationMarks(s) {
+  s = s.replace(/\!/g,'')
+  return s
+}
+*/
