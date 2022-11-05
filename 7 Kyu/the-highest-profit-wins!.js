dd4cf5acf933e42d678b954de9cf5f806e54cb9b
Tristan Maber <teejcoder@gmail.com>
2022-11-05 14:36:01 +1100

    Create the-highest-profit-wins!.js

---
+++ 7 Kyu/the-highest-profit-wins!.js
@@ -0,0 +1,16 @@
+/* DESCRIPTION
+Write a function that returns both the minimum and maximum number of the given list/array. 
+*/
+
+
+/*
+MY CODE
+function minMax(arr){
+  
+  let min = Math.min(...arr)
+  let max = Math.max(...arr)
+  
+  return [min, max]
+  
+}
+*/
