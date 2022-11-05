86d32c14a0adc4d5fcab9773f169c226fe19fdee
Tristan Maber <teejcoder@gmail.com>
2022-11-05 14:51:51 +1100

    Create list-filtering.js

---
+++ 7 Kyu/list-filtering.js
@@ -0,0 +1,12 @@
+/* DESCRIPTION
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+*/
+
+
+/* MY CODE
+
+function filter_list(l) {
+return l.filter(value => typeof value === 'number')                   
+}
+
+*/
