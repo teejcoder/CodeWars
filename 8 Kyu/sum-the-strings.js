9b49437303623fff4e71c1bce752c2f00c5f85ac
Tristan Maber <teejcoder@gmail.com>
2022-11-29 10:49:37 +1100

    Create sum-the-strings.js

---
+++ 8 Kyu/sum-the-strings.js
@@ -0,0 +1,10 @@
+/* DESCRIPTION
+Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+*/
+
+/* MY CODE
+function sumStr(a,b) {
+  let sum = +a + +b
+  return sum.toString()
+}
+*/
