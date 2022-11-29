0b0941a6f6b0407e4d506ce90e15040182e72f18
Tristan Maber <teejcoder@gmail.com>
2022-11-29 11:50:31 +1100

    Create bug-fixing.js

---
+++ 8 Kyu/bug-fixing.js
@@ -0,0 +1,9 @@
+/* DESCRIPTION
+Oh no! Timmy hasn't followed instructions very carefully and forgot how to use the new String Template feature, Help Timmy with his string template so it works as he expects!
+*/
+
+/* MY CODE
+function buildString(...template){
+  return `I like ${template.join(', ')}!`;
+}
+*/
