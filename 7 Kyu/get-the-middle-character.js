e55273ad9054ec63fa936cc92e58a514db94cc7c
Tristan Maber <teejcoder@gmail.com>
2022-11-30 15:44:26 +1100

    Create get-the-middle-character.js

---
+++ 7 Kyu/get-the-middle-character.js
@@ -0,0 +1,9 @@
+/* DESCRIPTION
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+*/
+
+/* MY CODE
+function getMiddle(s){
+  return s.slice((s.length-1) /2, (s.length /2) +1)
+}
+*/
