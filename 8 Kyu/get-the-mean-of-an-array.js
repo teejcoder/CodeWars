435d94bbb8837b852d91e2803bf713fb2d54b2c6
Tristan Maber <teejcoder@gmail.com>
2022-11-30 15:10:01 +1100

    Create get-the-mean-of-an-array.js

---
+++ 8 Kyu/get-the-mean-of-an-array.js
@@ -0,0 +1,10 @@
+/*
+Return the average of the given array rounded down to its nearest integer.
+*/
+
+/* MY CODE
+function getAverage(marks){
+  let sum = marks.reduce((acc,c) => acc + c)
+  return Math.floor(sum / marks.length)
+}
+*/
