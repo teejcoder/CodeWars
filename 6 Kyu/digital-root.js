98005176bd6a58098905db283e043b5a740a0bd6
Tristan Maber <teejcoder@gmail.com>
2022-12-01 14:52:29 +1100

    Create digital-root.js

---
+++ 6 Kyu/digital-root.js
@@ -0,0 +1,9 @@
+/* DESCRIPTION
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+*/
+
+/* MY CODE
+function digitalRoot(n) {
+  return (n - 1) % 9 + 1
+}
+*/
