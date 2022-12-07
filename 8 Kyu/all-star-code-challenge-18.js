5768c40d504f34ddc92a4128b8af2470bbb664fb
Tristan Maber <teejcoder@gmail.com>
2022-12-07 19:54:05 +1100

    Create all-star-code-challenge-18.js

---
+++ 8 Kyu/all-star-code-challenge-18.js
@@ -0,0 +1,9 @@
+/* DESCRIPTION
+Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+*/
+
+/* MY CODE
+function strCount(str, letter){  
+  return str.split(letter).length -1
+}
+*/
