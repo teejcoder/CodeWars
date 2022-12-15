c3369f52ff2a8129a554cdb514209ea3686aa10b
Teej <96551689+teejcoder@users.noreply.github.com>
2022-12-15 14:18:17 +1100

    Create vowelRemover.js

---
+++ 8 Kyu/vowelRemover.js
@@ -0,0 +1,9 @@
+/* DESCRIPTION
+Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+*/
+
+/* MY CODE
+function shortcut (string) {
+  return string.replace(/[aeiou]/g,  "")
+}
+*/
