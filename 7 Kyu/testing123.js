803cc0e7ebe475a959cb5914ea38d091275e270b
Teej <96551689+teejcoder@users.noreply.github.com>
2022-12-19 16:29:19 +1100

    Create testing123.js

---
+++ 7 Kyu/testing123.js
@@ -0,0 +1,16 @@
+/* DESCRIPTION
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+*/
+
+/* MY CODE
+const number = (array) => array.map((i, e) => `${++e}: ${i}`)
+*/
